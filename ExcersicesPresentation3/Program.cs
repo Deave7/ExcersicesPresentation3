8f668bdac774ec7291e12df23cb75969c046530a
Gustav Syrén <g.e.syren@gmail.com>
2023-08-08 22:02:58 +0200

    2

--- ExcersicesPresentation3/Program.cs
+++ ExcersicesPresentation3/Program.cs
@@ -308,24 +308,196 @@
 
         static void Exempel8() //Övning 8
         {
-            Console.WriteLine("Please pick one of the following to input: ");
-            Console.WriteLine("[I]nt");
-            Console.WriteLine("[D]ouble");
-            Console.WriteLine("[S]tring");
-            string choice = Console.ReadLine().ToUpper();
-          
-            switch (choice)
+            while (true) //Loopar för evigt. Tillagt för att man ska få chansen att välja igen och fortsätta testa utan att hela tiden starta om programmet 
             {
-                case "I":
-                    Console.WriteLine("Please enter your int: "); 
-                    break;
+                //Skriver ut en meny 
+                Console.WriteLine("Please pick one of the following to input: ");
+                Console.WriteLine("[I]nt");
+                Console.WriteLine("[D]ouble");
+                Console.WriteLine("[S]tring");
+                string choice = Console.ReadLine().ToUpper(); //Sätter string choice till värdet som användaren ger och gör om värdet till en stor bokstav
+
+                switch (choice) //Switch sats som kollar på variablen choice)
+                {
+                    case "I": //Om I
+                    case "D": //Om D
+                        Console.WriteLine("Please enter your number: "); 
+                        double input = Convert.ToDouble(Console.ReadLine()); //Initierar och sätter värdet till variablen input. Omvandlar användarens svar från en string till en double 
+                        input++; //Lägger till +1 till inputs värde
+                        Console.WriteLine(input + "\n"); //Skriver ut input och gör en ny rad
+                        break; //går ur switch satsen 
+
+                    case "S": //Om S
+                        Console.WriteLine("Please enter your string: ");
+                        string input2 = Console.ReadLine();
+                        input2 += "*";
+                        Console.WriteLine(input2 + "\n");
+                        break;
+
+                    default: //Om varken I, D eller S 
+                        Console.WriteLine("Please enter one of the choices\n");
+                        break;
+                }
+            }
+        }
+
+        static void Exempel9() //Övning 9
+        {
+            Console.WriteLine("Please input five numbers: ");
+
+            //Initierar variabler att spara användar input i 
+           
+            int numOne = Convert.ToInt32(Console.ReadLine());           
+            int numTwo = Convert.ToInt32(Console.ReadLine());           
+            int numThree = Convert.ToInt32(Console.ReadLine());          
+            int numFour = Convert.ToInt32(Console.ReadLine());
+            int numFive = Convert.ToInt32(Console.ReadLine());
+
+            bool subsetSumZeroExists = false;
+
+            //Kollar om någon version av de fem talen leder till att summan blir 0. Lite löjligt utan loopar och andra saker. 
+            if (numOne + numTwo + numThree + numFour + numFive == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numOne + numTwo + numThree + numFour == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numOne + numTwo + numThree + numFive == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numOne + numTwo + numFour + numFive == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numOne + numThree + numFour + numFive == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numTwo + numThree + numFour + numFive == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numOne + numTwo + numThree == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numOne + numTwo + numFour == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numOne + numTwo + numFive == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numOne + numThree + numFour == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numOne + numThree + numFive == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numOne + numFour + numFive == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numTwo + numThree + numFour == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numTwo + numThree + numFive == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numTwo + numFour + numFive == 0)
+            {
+                subsetSumZeroExists = true;
+            }
+            else if (numThree + numFour + numFive == 0)
+            {
+                subsetSumZeroExists = true;
             }
 
+            if (subsetSumZeroExists)
+            {
+                Console.WriteLine("There exists a subset of numbers that equals 0.");
+            }
+            else
+            {
+                Console.WriteLine("There is no subset of numbers that equals 0.");
+            }
+        }
+
+        static void Exempel10() //Övning 10
+        {
+            
+            Console.WriteLine("Please enter your points! [1-9]: "); //Ber användaren om input
+            int points = Convert.ToInt32(Console.ReadLine()); //Initierar variablen points och sätter värdet till användarinput. Konverterar användarens svar från en string till en int
+
+            switch (points)
+            {
+                case 1: //Om 1 
+                case 2: //Om 2
+                case 3: //Om 3
+                    int result = points * 10; //intiera variablen result och sätt dess värdet till points multiplicerat med 10 
+                    Console.WriteLine("Your new total is: " + result); //Skriv ut 
+                    break; //gå ur switch satsen 
+
+                case 4: 
+                case 5:
+                case 6:
+                    int resultTwo = points * 100;
+                    Console.WriteLine("Your new total is: " + resultTwo);
+                    break;
+                case 7:
+                case 8:
+                case 9:
+                    int resultThree = points * 1000;
+                    Console.WriteLine("Your new total is: " + resultThree);
+                    break;
+
+                default: //Om man ej skriver en siffra mellan 1 och 9 
+                    Console.WriteLine("Please enter a number between 1 and 9");
+                    break;
+            }
+        }
+
+        static void Exempel11() //Övning 11
+        {
+            Console.WriteLine("Please enter a number between 1-999: ");
+            int number = int.Parse(Console.ReadLine()); //Initierar och sätter värdet till det som användaren skrivit 
+
+            string result = ""; //initierar variablen result till en tom sträng 
+
+            if (number == 0) //Om variablen number är likamed 0 
+            {
+                result = "Zero"; //Skriv ut 
+            }
+            else //Annars 
+            {
+                //intierar fält med värden för att skriva ut siffror med ord 
+                string[] onesPlace = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" }; //börjar med en tom sträng för att de andra värdena ska hamna på rätt plats 
+                string[] tensPlace = { "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+                string[] teenNumbers = { "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+
+                int hundreds = number / 100; //initierar variablen hundreds och sätter värdet hur många 100-tal vi har. (genom uträkningen) 
+                int tens = (number / 10) % 10; //initierar variablen tens och sätter värdet hur många 10-tal vi har. (genom uträkningen) 
+                int ones = number % 10; //initierar variablen ones och sätter värdet hur många 1-tal vi har. (genom uträkningen) 
+
+                result += hundreds > 0 ? onesPlace[hundreds] + " Hundred" : ""; // OM hundreds är större än 0 körs det som är bakom "?" alltså lägger vi till värdet från fältet onesPlace[värdet på hundreds] + ordet Hundred och ett mellanslag
+                result += tens > 1 ? " " + tensPlace[tens - 1] : (tens == 1 ? " " + teenNumbers[ones - 1] : ""); //OM tens är större än 1 körs det bakom "?" alltså lägger vi till ett mellanslag + värdet på fältet tensPlace[värdet på tens - 1]. OM tens är 1 så lägger vi istället till mellanslag + värdet på fältet teenNumbers[värdet på ones -1
+                result += tens != 1 && ones > 0 ? " " + onesPlace[ones] : ""; //Om tens INTE är 1 och ones är större än 0 körs det bakom "?" alltså lägger vi till mellanslag + värdet från fältet onesPlace[värdet på ones] + mellanslag 
+            }
+
+            Console.WriteLine("Number in words: " + result);
         }
 
         static void Main(string[] args)
         {
-            Exempel3();
+            
         }
     }
 }
